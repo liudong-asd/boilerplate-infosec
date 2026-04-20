4d75df20f61b0ca52a1e4fb36adc8c71f992b10a
liudong-asd <1792084140@qq.com>
2026-04-20 18:56:48 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const helmet = require('helmet');
-
 const app = express();
 
+// 开启Helmet安全防护
 app.use(helmet());
 
+// 加一个根路由，不然访问会404
+app.get('/', (req, res) => {
+  res.send('Hello Helmet! 服务运行正常');
+});
 
-
-
+// 监听3000端口
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`服务器已启动，访问 http://localhost:${PORT}`);
+});
 
 
 
